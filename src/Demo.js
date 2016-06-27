bafc52034593040915ec627caa9d0d12edf5608b
bogdanpetru <bogdanpintican@gmail.com>
2016-06-28 00:14:38 +0300

    Basic setup, email, name

--- src/Demo.js
+++ src/Demo.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import data from './data.js'
+import Resume from './Resume'
 
 export default class Demo extends Component {
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <Resume data={data} />
     );
   }
 }
--- src/Resume/index.js
+++ src/Resume/index.js
@@ -1,7 +1,59 @@
 import React, { Component } from 'react'
 
+const bemConstructor = base => ({ child, state }) => {
+  let className =  base;
+
+  if (child) {
+    className = `${className}__${child}`
+  }
+
+  if (state) {
+    className = `${className}--${state}`
+  }
+
+  return className;
+}
+
+const rootBEM = bemConstructor('react-resume')
+
+const Name = ({ name }) => <h1
+    className={rootBEM({ child: 'name' })}>
+    {name}
+  </h1>
+
+const Label = ({ label }) => <h2
+    className={rootBEM({ child: 'label' })}>
+    {label}
+  </h2>
+
+const Email = ({ email }) => <span
+    className={rootBEM({ child: 'email' })}>
+    {email}
+  </span>
+
 class Resume extends Component {
   render() {
-    return <div>Hello world</div>
+    const {
+      data
+    } = this.props
+
+    const {
+      basics : {
+        name,
+        label,
+        picture,
+        email
+      }
+    } = data;
+
+    console.log(email)
+
+    return <div className="react-resume">
+      <Name name={name} />
+      <Label label={label} />
+      <Email label={email} />
+    </div>
   }
 }
+
+export default Resume
