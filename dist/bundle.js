caf7348fc304429a30d5f946c50dadfad79dd3e2
bogdanpetru <bogdanpintican@gmail.com>
2016-07-24 18:47:03 +0300

    Save build

--- dist/bundle.js
+++ dist/bundle.js
@@ -65,7 +65,7 @@
 /******/ 	}
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "2f49220a73c4e5b2b224"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "4ef1627206211d0014ab"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 /******/ 	
@@ -570,7 +570,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/dist/";
+/******/ 	__webpack_require__.p = "./dist/";
 
 /******/ 	// __webpack_hash__
 /******/ 	__webpack_require__.h = function() { return hotCurrentHash; };
